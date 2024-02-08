e24e5459a161e5b72ce8305a3451a01b1915dbe0
Aman Kumar Singh <aman.singh@theporter.in>
2024-02-08 13:40:58 +0530

    adding files

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.aman.ktor-sonar-test"
---
+++ src/main/kotlin/com/aman/Application.kt
@@ -0,0 +1,15 @@
+package com.aman
+
+import com.aman.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureHTTP()
+    configureRouting()
+}
